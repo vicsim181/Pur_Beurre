739f929ca7f131f3c1bf44115cb9f82d563fa56d
vicsim181 <63299151+vicsim181@users.noreply.github.com>
2020-11-07 13:25:35 +0100

    ORM complet 99%

--- Models/junction.py
+++ Models/junction.py
@@ -7,11 +7,5 @@ from models.base import Base
 class StoreProduct(Base):
     __tablename__ = "store_product"
 
-    id_product = Column(Integer, ForeignKey("Product.Id"), primary_key=True,
-                        nullable=False)
-    name_product = Column(String(100))
-    name_store = Column(String(100))
-    id_store = Column(Integer, ForeignKey("Store.Id"), primary_key=True)
-
-    product = relationship("Product")
-    store = relationship("Store")
+    id_product = Column(Integer, primary_key=True, nullable=False)
+    id_store = Column(Integer, primary_key=True)
--- Scripts/ormfeed.py
+++ Scripts/ormfeed.py
@@ -1,51 +1,80 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 """Script feeding the tables, using the SQLAlchemy ORM."""
+import json
+import requests
 import sys
-sys.path.append('D:\Github\P5\github')
-import json, requests
+from sqlalchemy import and_, func
+from sqlalchemy.orm import sessionmaker
+sys.path.append("D:/Github/P5/github")
 from models.product import Product
-from models.favorite import Favorite
 from models.category import Category
 from models.junction import StoreProduct
-from models.base import Base, eng
+from models.base import eng
 from models.store import Store
-import mysql.connector
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 
-# eng = create_engine('mysql://root:japon+71@#cclmvdf@localhost/essai')#, echo=True)
+"""Initializing the session."""
 Session = sessionmaker(bind=eng)
 ses = Session()
 
+"""Extracting some parameters from the settings.json file."""
 with open('../models/settings.json', 'r') as settings:
     data = json.load(settings)
 
+"""Starting the extraction from the OpenFoodFacts API,\
+   feeding the tables Product and Store with part of the data collected."""
 i = 1
-junction = []
-
 categories = ses.query(Category)
 for category in categories:
-    url = f'''https://world.openfoodfacts.org/cgi/search.pl?search_terms={category.name}&search_simple=1&action=process&json=1&page={data['page']}&page_size={data['page_size']}'''
+    url = f'''https://world.openfoodfacts.org/cgi/search.pl?search_terms=\
+    {category.name}&search_simple=1&action=process&json=1&page={data['page']}&\
+    page_size={data['page_size']}'''
     json = requests.get(url).json()
     raw_products = json['products']
     for raw_product in raw_products:
-        product = Product(name=raw_product['product_name'], quantity=raw_product['quantity'], id_category=category.Id,
-        nutri_score=raw_product['nutriscore_grade'], ingredients=raw_product['ingredients_text_fr'], link_url=raw_product['url'], stores=str(raw_product['stores_tags']))
+        product = Product(name=raw_product['product_name'],
+                          quantity=raw_product['quantity'],
+                          id_category=category.Id,
+                          nutri_score=raw_product['nutriscore_grade'],
+                          ingredients=raw_product['ingredients_text_fr'],
+                          link_url=raw_product['url'],
+                          stores=str(raw_product['stores_tags']))
         ses.add(product)
         if raw_product['stores_tags']:
             for store in raw_product['stores_tags']:
+                pr_name = raw_product['product_name']
                 store_add = Store(name=store)
                 ses.add(store_add)
         i += 1
 ses.commit()
 
-# pr = ses.query(Product).filter(Product.name==raw_product['product_name'])
-#                 id_pr = pr.Id
+"""Deleting the duplicates in the Store table."""
+subq = (
+       ses.query(Store.name, func.min(Store.Id).label("min_id"))
+       .group_by(Store.name)
+) .subquery('name_min_id')
+duplicates_store = (ses.query(Store).join(subq,
+                    and_(Store.name == subq.c.name, Store.Id != subq.c.min_id))
+                    )
+for dup in duplicates_store:
+    ses.delete(dup)
+ses.commit()
 
-# pr_name = raw_product['product_name']
-#                 pr = ses.query(Product).filter(Product.name == f'{pr_name}').first()
-#                 stor = ses.query(Store).filter(Store.name == f'{store}').first()
-#                 jnct_st_pr = StoreProduct(id_product=pr.Id, name_product=(f'{pr_name}'), name_store=store, id_store=stor.Id)
-#                 j += 2
-#                 ses.add(jnct_st_pr)
+"""Cleaning the stores column of the Product table."""
+junction = []
+products = ses.query(Product)
+for product in products:
+    if product.stores != '[]':
+        detail = product.stores.split(',')
+        for element in detail:
+            element = element.replace("'", "").replace('[', '').replace(']', '').strip()
+            stores = ses.query(Store).filter(Store.name == f'{element}').first()
+            junction.append([product.Id, product.name, element, stores.Id])
+
+"""Inserting the data in the junction table: Store_Product,\
+   with only the Ids of the products and stores."""
+for line in junction:
+    add_to_junction = StoreProduct(id_product=f'{line[0]}',
+                                   id_store=f'{line[3]}')
+    ses.add(add_to_junction)
+ses.commit()

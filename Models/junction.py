e4ad86125007e525fdbf0947197f5152865b64ba
vicsim181 <63299151+vicsim181@users.noreply.github.com>
2020-11-30 14:42:19 +0100

    ajustement lignes

--- Models/junction.py
+++ Models/junction.py
@@ -6,7 +6,5 @@ from Models.base import Base
 class StoreProduct(Base):
     __tablename__ = "store_product"
 
-    id_product = Column(Integer, ForeignKey("Product.Id", ondelete="CASCADE"),
-                        primary_key=True, nullable=False)
-    id_store = Column(Integer, ForeignKey("Store.Id", ondelete="CASCADE"),
-                      primary_key=True)
+    id_product = Column(Integer, ForeignKey("Product.Id", ondelete="CASCADE"), primary_key=True, nullable=False)
+    id_store = Column(Integer, ForeignKey("Store.Id", ondelete="CASCADE"), primary_key=True)

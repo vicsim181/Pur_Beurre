fe1493f861bcd95e745144470dbba54a49e759fa
vicsim181 <63299151+vicsim181@users.noreply.github.com>
2020-12-31 14:26:52 +0100

    Update base.py

--- Models/base.py
+++ Models/base.py
@@ -3,8 +3,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 import os
 
-password = os.environ['MySQL_password']
+password = os.environ['MySQL_password_student']
 
-eng = create_engine(f'mysql://root:{password}@localhost/purbeurre?charset=utf8mb4')
+eng = create_engine(f'mysql://student:{password}@localhost/purbeurre?charset=utf8mb4')
 
 Base = declarative_base()
